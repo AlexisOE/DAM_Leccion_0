92926a93e245cbf8975bf3cd45db807a9cd5e540
AlexisOE <21030319@itesa.edu.mx>
2025-01-28 18:55:01 -0600

    Versión_02: Instrucciones básicas en botón y caja de texto.

--- DAM_Leccion_AOE/MainPage.xaml.cs
+++ DAM_Leccion_AOE/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace DAM_Leccion_AOE
+﻿using DAM_Leccion_AOE.Model;
+
+namespace DAM_Leccion_AOE
 {
     public partial class MainPage : ContentPage
     {
@@ -7,6 +9,7 @@
         public MainPage()
         {
             InitializeComponent();
+            Ejecutar();
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -19,6 +22,18 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        public void Ejecutar()
+        {
+            PersonaModel personaModel = new PersonaModel();
+            personaModel.Nombre = "Hola aqui estoy";
+            txtNombre.Text = "Hola aqui estoy";
+        }
+
+        private void btnAceptar_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Asistente del sistema", "Se ha guardado el registro en la BD", "Aceptar");
         }
     }
 
